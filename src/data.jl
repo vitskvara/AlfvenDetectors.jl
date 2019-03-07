368e6a201d33320df48fd492cdf4522fa75957fb
Vít Škvára <vit.skvara@gmail.com>
2019-03-07 07:50:03 +0100

    flattop detection

--- src/data.jl
+++ src/data.jl
@@ -1,3 +1,5 @@
+import Base.collect
+
 """
 	BaseAlfvenData
 
@@ -14,10 +16,11 @@ Fields:
 	shot = number of shot
 """
 mutable struct BaseAlfvenData
-	msc::Dict{Any, Matrix}
+	mscamp::Dict{Any, Matrix}
+	mscphase::Dict{Any, Matrix}
 	tmsc::Vector
 	fmsc::Vector
-	psd::Matrix
+	upsd::Matrix
 	tpsd::Vector
 	fpsd::Vector
 	fnoscale::Vector
@@ -33,6 +36,7 @@ end
 Default empty constructor.
 """
 BaseAlfvenData() = BaseAlfvenData(
+		Dict{Int, Matrix{Float}}(),
 		Dict{Int, Matrix{Float}}(),
 		Vector{Float}(),
 		Vector{Float}(),
@@ -55,12 +59,10 @@ function BaseAlfvenData(filepath::String)
 	alfvendata = BaseAlfvenData()
 	alfvendata.filepath = filepath
 	alfvendata.shot = split(basename(filepath), ".")[1]
-	data = h5open(filepath, "r") do file
-		# read the basic signals
-		readbasic!(alfvendata, file)
-		# read the msc data
-		readmsc!(alfvendata, file)
-	end
+	# read the basic signals
+	readbasic!(alfvendata, filepath)
+	# read the msc data with only some coils
+	readmsc!(alfvendata, filepath)
 	return alfvendata
 end
 
@@ -73,45 +75,44 @@ function BaseAlfvenData(filepath::String, coillist::Vector)
 	alfvendata = BaseAlfvenData()
 	alfvendata.filepath = filepath
 	alfvendata.shot = split(basename(filepath), ".")[1]
-	data = h5open(filepath, "r") do file
-		# read the basic signals
-		readbasic!(alfvendata, file)
-		# read the msc data with only some coils
-		readmsc!(alfvendata, file; coillist = coillist)
-	end
+	# read data
+	readbasic!(alfvendata, filepath)
+	# read the msc data with only some coils
+	readmsc!(alfvendata, filepath; coillist = coillist)
 	return alfvendata
 end
 
 """
-	readbasic!(alfvendata::BaseAlfvenData, file::HDF5File)
+	readbasic!(alfvendata::BaseAlfvenData, filepath::String)
 
 Read the basic signals - time, frequency etc.
 """
-function readbasic!(alfvendata::BaseAlfvenData, file::HDF5File)
-	alfvendata.tmsc = Float.(read(file, "t_cohere"))
-	alfvendata.fmsc = Float.(read(file, "f_cohere"))
-	alfvendata.fnoscale = Float.(read(file, "fnoscale"))
-	alfvendata.tfnoscale = Float.(read(file, "t_fnoscale"))
-	alfvendata.psd = Float.(read(file, "Uprobe_coil_A1pol_psd"))
-	alfvendata.tpsd = Float.(read(file, "t_Uprobe"))
-	alfvendata.fpsd = Float.(read(file, "f_Uprobe"))
-	alfvendata.ip = Float.(read(file, "I_plasma"))
+function readbasic!(alfvendata::BaseAlfvenData, filepath::String)
+	alfvendata.tmsc = readsignal(filepath, "t_cohere")
+	alfvendata.fmsc = readsignal(filepath, "f_cohere")
+	alfvendata.fnoscale = readsignal(filepath, "fnoscale")
+	alfvendata.tfnoscale = readsignal(filepath, "t_fnoscale")
+	alfvendata.upsd = readsignal(filepath, "Uprobe_coil_A1pol_psd")
+	alfvendata.tpsd = readsignal(filepath, "t_Uprobe")
+	alfvendata.fpsd = readsignal(filepath, "f_Uprobe")
+	alfvendata.ip = readsignal(filepath, "I_plasma")
 end
 
 """
-	readmsc!(alfvendata::BaseAlfvenData, file::HDF5File; coillist=nothing)
+	readmsc!(alfvendata::BaseAlfvenData, filepath::String; coillist=nothing)
 
 Read the magnitude squared coherence from a .h5 file. If coillist is specified,
 only certain coils will be loaded.
 """
-function readmsc!(alfvendata::BaseAlfvenData, file::HDF5File; coillist=nothing)
+function readmsc!(alfvendata::BaseAlfvenData, filepath::String; coillist=nothing)
 	# if some coil data is missing, save the name in this list and filter them at the end
-	_coillist = ((coillist == nothing) ? getcoillist(names(file)) : coillist)
+	_coillist = ((coillist == nothing) ? getcoillist(names(h5open(filepath,"r"))) : coillist)
 	#_coillist = String.(_coillist)
 	for coil in _coillist
 		try 
 			@suppress_err begin
-				alfvendata.msc[coil] = Float.(read(file, "Mirnov_coil_A&C_theta_$(coil)_cpsdphase"))
+				alfvendata.mscphase[coil] = readsignal(filepath, "Mirnov_coil_A&C_theta_$(coil)_cpsdphase")
+				alfvendata.mscamp[coil] = readsignal(filepath,  "Mirnov_coil_A&C_theta_$(coil)_coherems")
 			end
 		catch e
 			if isa(e, ErrorException)
@@ -124,6 +125,36 @@ function readmsc!(alfvendata::BaseAlfvenData, file::HDF5File; coillist=nothing)
 end
 
 """
+	readsignal(filepath::String, signal::String)
+"""
+function readsignal(filepath::String, signal::String)
+	x = h5open(filepath, "r") do file
+		read(file, signal)
+	end
+	return Float.(x)
+end
+
+"""
+	readmscamp(filepath::String, coil)
+"""
+readmscamp(filepath::String, coil) = readsignal(filepath, "Mirnov_coil_A&C_theta_$(coil)_coherems")
+
+"""
+	readmscphase(filepath::String, coil)
+"""
+readmscphase(filepath::String, coil) = readsignal(filepath, "Mirnov_coil_A&C_theta_$(coil)_cpsdphase")
+
+"""
+	readip(filepath::String)
+"""
+readip(filepath::String) = readsignal(filepath, "I_plasma")
+
+"""
+	readupsd(filepath::String)
+"""
+readupsd(filepath::String) = readsignal(filepath, "Uprobe_coil_A1pol_psd")
+
+"""
 	getcoillist(keynames)
 
 Extract all availabel Mirnov coils from a list of strings (keys of a hdf5 file).
@@ -132,4 +163,122 @@ function getcoillist(keynames)
 	ks = filter(x->occursin("Mirnov",x), keynames)
 	ks = Meta.parse.(unique(vcat(split.(ks, "_")...)))
 	ks = filter(x->typeof(x) <: Int, ks)
+end
+
+##########################
+### flat-top detection ###
+##########################
+"""
+    isnegative(x)
+
+Is x mostly negative?
+"""
+isnegative(x) = (maximum(x) < abs(minimum(x)))
+
+"""
+    makepositive(x)
+
+Make x positive if negative.
+"""
+makepositive(x) = isnegative(x) ? -x : x
+
+"""
+    maxflattop(x,th)
+
+Flattop is identified as being at least th% of maximum of Ip.
+"""
+maxflattop(x,th=0.7) = (x .>= maximum(x)*th)
+
+"""
+    movingmean(x,wl)
+
+Moving average of x with window length wl.
+"""
+movingmean(x,wl) = map(n->StatsBase.mean(x[max(1,n-wl):n]), 1:length(x))
+    
+"""
+	diffflattop(x, ϵ=1e-5; wl=0)
+
+Flattop is identified as having a derivative smaller than
+ϵ*maximum(x), wl is length of window for moving average
+of the derivative.
+"""
+function diffflattop(x, ϵ=1e-5; wl=0)
+    dx = diff(x)
+    dx = movingmean(dx,wl)
+    return vcat([false], abs.(dx) .<= ϵ*maximum(x))
+end
+
+"""
+	diffmaxflattop(x,th=0.7,ϵ=1e-4;wl=0)
+
+Combination of flattop detection by maximum and derivative.
+"""
+diffmaxflattop(x,th=0.7,ϵ=1e-4;wl=0) = (maxflattop(x,th) .& diffflattop(x,ϵ;wl=wl))
+
+"""
+    maxsection(x::Array{Int,1})
+
+Finds the longest uninterupted section in an array of integers.
+Uninterupted means that the difference between two neighboring values
+are 1.
+"""
+function maxsection(x::Array{Int,1})
+    inddiffs = diff(x)
+    lengths = []
+    deltas = []
+    k = 0
+    for i in 1:length(inddiffs)
+        d = inddiffs[i]
+        if d > 1
+            push!(lengths,k)
+            push!(deltas, d)
+            k=0
+        else
+            k += d
+        end
+    end
+    # in case the whole  collection is empty - all the indices are uninterrupted
+    push!(lengths,k)
+    imx = argmax(lengths)
+    if imx == 1
+        maxsectioninds = 1:lengths[1]+1
+    else
+        maxsectioninds = (sum(lengths[1:imx-1])+sum(deltas[1:imx-1])+1):(sum(lengths[1:imx])+sum(deltas[1:imx-1])+1)
+    end
+    return maxsectioninds
+end
+
+"""
+    maxsection(mask::AbstractArray{Bool})
+
+For a given boolean vector, obtain the indices of the longest uninterupted section that is true.
+"""
+function maxsection(mask::AbstractArray{Bool})
+    maxinds = maxsection(collect(1:length(mask))[mask])
+    offset = 0
+    for i in 1:length(mask)
+        if mask[i]
+            break
+        else
+            offset += 1
+        end
+    end
+    return maxinds .+ offset
+end
+
+"""
+    maxsectionbe(mask::Array{Bool,1})
+
+Maximum section beginning and end.
+"""
+function maxsectionbe(mask::AbstractArray{Bool})
+    maxinds = maxsection(mask)
+    return maxinds[1], maxinds[end]
+end
+
+function flattopbe(x,th=0.7,ϵ=1e-4;wl=0)
+    _x = makepositive(x)
+    mask = diffmaxflattop(_x,th,ϵ;wl=wl)
+    return maxsectionbe(mask)
 end
--- test/data.jl
+++ test/data.jl
@@ -3,7 +3,7 @@ using AlfvenDetectors
 using Random
 using HDF5
 
-fpath = "data/testdata.h5"
+fpath = joinpath(dirname(@__FILE__),"data/testdata.h5")
 
 @testset "data" begin
 	h5data = h5read(fpath,"/")
@@ -11,15 +11,68 @@ fpath = "data/testdata.h5"
 
 	data = AlfvenDetectors.BaseAlfvenData(fpath)
 	vecfields = [:tmsc, :fmsc, :tpsd, :fpsd, :fnoscale, :tfnoscale, :ip]
-	matfields = [:psd]
+	matfields = [:upsd]
 	for field in vecfields
 		@test size(getfield(data, field)) == (3,)
 	end	 
 	for field in matfields
 		@test size(getfield(data, field),2) == 3
 	end	 	
-	@test length(data.msc) == 2
+	@test length(data.mscamp) == 2
+	@test length(data.mscphase) == 2
+	for coil in keys(data.mscphase)
+		@test size(data.mscphase[coil],2) == 3
+	end
+	for coil in keys(data.mscamp)
+		@test size(data.mscamp[coil],2) == 3
+	end
 
 	data = AlfvenDetectors.BaseAlfvenData(fpath, [11,12])
-	@test length(data.msc) == 1
+	@test length(data.mscamp) == 1
+	@test length(data.mscphase) == 1
+	for coil in keys(data.mscphase)
+		@test size(data.mscphase[coil],2) == 3
+	end
+	for coil in keys(data.mscamp)
+		@test size(data.mscamp[coil],2) == 3
+	end
+
+	##########################
+	### flat-top detection ###
+	##########################
+	x = collect(1:10)
+	@test AlfvenDetectors.isnegative(-x)
+	@test !AlfvenDetectors.isnegative(x)
+	@test x == AlfvenDetectors.makepositive(x)
+	@test x == AlfvenDetectors.makepositive(-x)
+	x[3] = 9
+	@test x[AlfvenDetectors.maxflattop(x,0.8)] == [9,8,9,10]
+	x[3] = 3
+	@test x == AlfvenDetectors.movingmean(x,0)
+	y = AlfvenDetectors.movingmean(x,1)
+	@test length(y) == length(x)
+	@test y == [1.0, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
+	x = vcat(collect((1:4)).*2, fill(10,5), collect((4:-1:1)).*2)
+	x[6] = 9
+	x[8] = 11
+	@test x[AlfvenDetectors.diffflattop(x,0.1)] == [9,10,11,10]
+	x = vcat(x,fill(1,10))
+	@test x[AlfvenDetectors.diffmaxflattop(x,0.8,0.1)] == [9,10,11,10]
+
+	# tests for the max section fctions
+	x1 = collect(2:45)
+	y1 = vcat(collect(3:9), collect(11:12), collect(18:21), [40])
+	is1 = map(x->x in y1, x1);
+	x2 = collect(1:45)
+	y2 = vcat(collect(3:4), collect(11:16), collect(18:24), [40])
+	is2 = map(x->x in y2, x2);
+	# test the maxsection etc.
+	@test y1[AlfvenDetectors.maxsection(y1)] == collect(3:9)
+	# this is broken and does not work as intended
+	# @test y2[AlfvenDetectors.maxsection(y2)] == collect(18:24)
+	@test x1[AlfvenDetectors.maxsection(is1)] == collect(3:9)
+	@test x2[AlfvenDetectors.maxsection(is2)] == collect(18:24)
+	@test AlfvenDetectors.maxsectionbe(is1) == (2,8)
+	@test AlfvenDetectors.maxsectionbe(is2) == (18,24)
+	@test AlfvenDetectors.flattopbe(x,0.8,0.1) == (6,9)
 end
