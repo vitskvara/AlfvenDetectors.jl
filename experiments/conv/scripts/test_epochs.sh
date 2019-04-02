9265b89a1460f762703e342a15dd0c7763a55972
Vít Škvára <vit.skvara@gmail.com>
2019-04-02 14:57:25 +0200

    conv scripts

---
+++ experiments/conv/scripts/test_epochs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# run10mscmodels.sh modelname ldim nlayers
+for nepochs in {1 2 5 10 20 50 100}
+do
+	julia ../run_experiment.jl TSVAE 32 4 8 16 32 64 --nepochs=$nepochs --patchsize=128 --gpu --memory-efficient --no-warnings
+done
