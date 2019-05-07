4b81398f31944ba7b741300e8e1547b30a716915
Vít Škvára <vit.skvara@gmail.com>
2019-05-07 17:01:43 +0200

    added run scripts for 3d models

---
+++ experiments/conv/scripts/run_aae_3d.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for seed in 1 2 3 4 5 6 7 8 9 10
+do
+	julia run_experiment.jl AAE 3 3 8 16 32 --scaling 2 2 1 --gpu --memory-efficient --memorysafe --savepath=aae_3d_$seed --nepochs=200 --savepoint=40 --ndense=3 --hdim=64 --positive-patch-ratio=0.2 --disc-nlayers=3 --pz-components=8 --eta=0.001 --batchnorm --seed=$seed
+done
