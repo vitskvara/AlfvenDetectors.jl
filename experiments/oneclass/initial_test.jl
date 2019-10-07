f4261c583bbd940adc8c9f2ff86355dd31471d6a
Vit Skvara <vit.skvara@gmail.com>
2019-10-07 10:44:01 +0200

    gpu node version of init script

--- experiments/oneclass/initial_test.jl
+++ experiments/oneclass/initial_test.jl
@@ -13,38 +13,49 @@ warnings = true
 iptrunc = "valid"
 memorysafe = true # if true, slower but enables loading of more data - the Julia h5 library has a memory leak
 readfun = AlfvenDetectors.readnormlogupsd
+readfun = AlfvenDetectors.readlogupsd
+
+
 seed = 1
 α = 0.8 # training/all patches ratio
-datapath = "/home/vit/vyzkum/alfven/cdb_data/uprobe_data"
-readfun = AlfvenDetectors.readnormlogupsd
+hostname = gethostname()
+if hostname == "gpu-node"
+	datapath = "/compass/Shared/Exchange/Havranek/Link to Alfven"
+else
+	datapath = "/home/vit/vyzkum/alfven/cdb_data/uprobe_data"
+end
 patchsize = 128
 
 # get the patch info
-all_info = AlfvenDetectors.labeled_patches()
-shotnos, patch_labels, tstarts, fstarts = all_info
+all_info = AlfvenDetectors.labeled_patches();
+shotnos, patch_labels, tstarts, fstarts = all_info;
 N = length(shotnos)
-positive_inds = (patch_labels .== 1)
+positive_inds = (patch_labels .== 1);
 N_positive = sum(positive_inds)
 N_train = floor(Int, N_positive*α)
-train_inds = sample(collect(1:N)[positive_inds], N_train)
-test_inds = collect(1:N)[map(n->!(n in train_inds),1:N)]
+train_inds = sample(collect(1:N)[positive_inds], N_train);
+test_inds = collect(1:N)[map(n->!(n in train_inds),1:N)];
 # now get the patches
-train_info = map(x->x[train_inds], all_info)
-test_info = map(x->x[test_inds], all_info)
+train_info = map(x->x[train_inds], all_info);
+test_info = map(x->x[test_inds], all_info);
 N_patches = 500
 train_patches = AlfvenDetectors.collect_training_patches(datapath, train_info[1], train_info[3], 
-	train_info[4], N_patches, readfun, patchsize; δ = 0.02, memorysafe = memorysafe)
+	train_info[4], N_patches, readfun, patchsize; δ = 0.02, memorysafe = memorysafe);
 train_data = train_patches[1] |> gpu
 # these are for the final model valiadation
 train_patches_validation = map(x->AlfvenDetectors.get_patch(datapath, x[1], x[2], x[3], patchsize, readfun; 
-		memorysafe = memorysafe)[1], zip(train_info[1], train_info[3], train_info[4]))
+		memorysafe = memorysafe)[1], zip(train_info[1], train_info[3], train_info[4]));
 train_patches_validation = cat(train_patches_validation..., dims=4)
 test_patches = map(x->AlfvenDetectors.get_patch(datapath, x[1], x[2], x[3], patchsize, readfun; 
-		memorysafe = memorysafe)[1], zip(test_info[1], test_info[3], test_info[4]))
+		memorysafe = memorysafe)[1], zip(test_info[1], test_info[3], test_info[4]));
 N_test = length(test_patches)
 test_patches = cat(test_patches..., dims=4)
 test_positive_inds = test_info[2] .== 1
 test_negative_inds = test_info[2] .== 0
+
+x = test_patches[:,:,1,end]
+maximum(x) # 0.6
+minimum(x) # 0.1
 
 # show some plots
 using PyPlot
@@ -59,26 +70,37 @@ end
 # 128 + upscale = 0.0076235738, 6:37
 # 128 + transpose = 0.005280155, 5:42
 # the precision seems to be the same, but one is a bit slower
+# 256 + upscale = 0.0040311976, 4 mins on gpu-node
 
 insize = size(train_data)[1:3]
-zdim = 256 #   loss:       0.0039325454
-zdim = 128 #   loss:       0.0043666917
+zdim = 1024 #   loss:       0.0039325454
+#zdim = 128 #   loss:       0.0043666917
 nconv = 2
 kernelsize = 3
 channels = (8, 16)
 scaling = (2, 2)
-upscale_type = "transpose"
+#upscale_type = "transpose"
 upscale_type = "upscale"
 model = ConvAE(insize, zdim, nconv, kernelsize, channels, scaling; upscale_type = upscale_type) |> gpu
 
 # and now train it
-opt = GenModels.fit!(model, train_data, 25, 20; cbit = 5, memoryefficient = true);
-opt = GenModels.fit!(model, train_data, 25, 20; opt=opt, cbit = 5, memoryefficient = true);
-opt = GenModels.fit!(model, train_data, 25, 20; opt=opt, cbit = 5, memoryefficient = true);
+if hostname == "gpu-node"
+	opt = GenModels.fit!(model, train_data, 50, 100; cbit = 5, memoryefficient = true);
+	opt = GenModels.fit!(model, train_data, 50, 100; opt=opt, cbit = 5, memoryefficient = true);
+else
+	opt = GenModels.fit!(model, train_data, 25, 20; cbit = 5, memoryefficient = true);
+	opt = GenModels.fit!(model, train_data, 25, 20; opt=opt, cbit = 5, memoryefficient = true);
+	opt = GenModels.fit!(model, train_data, 25, 20; opt=opt, cbit = 5, memoryefficient = true);
+end
 cpu_model = model |> cpu
 
 # save the model
-modelpath = "/home/vit/vyzkum/alfven/experiments/oneclass/initial_test"
+if hostname == "gpu-node"
+	modelpath = "/compass/home/skvara/alfven/experiments/oneclass/initial_test"
+else
+	modelpath = "/home/vit/vyzkum/alfven/experiments/oneclass/initial_test"
+end
+mkpath(modelpath)
 modelname = "ConvAE"
 fname = joinpath(modelpath, "$(modelname)_$(zdim)_$(upscale_type).bson")
 GenModels.save_model(fname, model |> cpu, 
