ed214b598e9440cfe3d1c016995a849e5e723ee2
Vit Skvara <vit.skvara@gmail.com>
2019-10-14 11:58:13 +0200

    fixed path in run script

--- experiments/oneclass/scripts/run.sh
+++ experiments/oneclass/scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/compass/home/skvara/julia-1.1.1/bin/julia ../run_experiment.jl AE 128 2 16 32 --savepath=first_runs --eta=0.0001 --nepochs=20 --batchnorm --resblock --verb
+~/julia-1.1.1/bin/julia ../run_experiment.jl WAE 128 2 16 32 --savepath=first_runs --eta=0.0001 --nepochs=20 --batchnorm --resblock --verb
