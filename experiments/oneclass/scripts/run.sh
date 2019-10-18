0e2a1266cd13ee646edfe41a56a2c33d5eb9e3ea
Vít Škvára <vit.skvara@gmail.com>
2019-10-18 15:46:01 +0200

    more runs

--- experiments/oneclass/scripts/run.sh
+++ experiments/oneclass/scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+~/julia-1.1.1/bin/julia ../run_experiment.jl AE 128 2 32 64 --kernelsize=5 --savepath=opt_runs/models --eta=0.0001 --nepochs=50 --batchnorm --resblock --verb --savepoint=50 --nonnormalized
 ~/julia-1.1.1/bin/julia ../run_experiment.jl AE 128 2 32 64 --kernelsize=5 --savepath=opt_runs/models --eta=0.0001 --nepochs=50 --batchnorm --resblock --verb --savepoint=50 --upscale-type=upscale
 ~/julia-1.1.1/bin/julia ../run_experiment.jl VAE 128 2 32 64 --kernelsize=5 --savepath=opt_runs/models --eta=0.0001 --nepochs=100 --batchnorm --resblock --verb --savepoint=50 --beta=1.0
 ~/julia-1.1.1/bin/julia ../run_experiment.jl VAE 128 2 32 64 --kernelsize=5 --savepath=opt_runs/models --eta=0.0001 --nepochs=100 --batchnorm --resblock --verb --savepoint=50 --beta=0.001
