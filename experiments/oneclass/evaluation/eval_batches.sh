4cee272cc9736b5b0ee5a5958ffe8c9eed386983
Vít Škvára <vit.skvara@gmail.com>
2019-10-24 15:13:51 +0200

    unsupervised

--- experiments/oneclass/evaluation/eval_batches.sh
+++ experiments/oneclass/evaluation/eval_batches.sh
@@ -1,6 +1,6 @@
 ~/julia-1.1.1/bin/julia eval_batch.jl opt_runs
 ~/julia-1.1.1/bin/julia eval_batch.jl negative_runs
 ~/julia-1.1.1/bin/julia eval_batch.jl eval_tuning
-~/julia-1.1.1/bin/julia eval_batch.jl nobatchnorm_tuning
+~/julia-1.1.1/bin/julia eval_batch.jl nobatchnorm_runs
 ~/julia-1.1.1/bin/julia eval_batch.jl ldim_runs
 ~/julia-1.1.1/bin/julia eval_batch.jl nobatchnorm_convsize_runs
