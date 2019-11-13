95246c9467ba0a2bc182ff6d9df8e9bf16f5f5f9
Vít Škvára <vit.skvara@gmail.com>
2019-11-13 17:49:33 +0000

    paper eval

--- experiments/paper/oneclass_eval.jl
+++ experiments/paper/oneclass_eval.jl
@@ -36,18 +36,26 @@ dfs = map(CSV.read, fs)
 bigdf = vcat(dfs...)
 
 # now lets compute the averages
-subcols = [:model,:channels,:ldim,:nepochs,:normalized,:neg,:λ,:γ,:σ,:auc_mse,:auc_mse_pos]
+metric = "auc_mse"
+metric_short = Symbol("auc")
+m1 = Symbol(metric)
+m2 = Symbol(metric*"_pos")
+m1m = Symbol(metric*"_mean")
+m1s = Symbol(metric*"_std")
+m2m = Symbol(metric*"_pos_mean")
+m2s = Symbol(metric*"_pos_std")
+subcols = [:model,:channels,:ldim,:nepochs,:normalized,:neg,:β,:λ,:γ,:σ,m1,m2]
 subdf = bigdf[!,subcols]
-agcols = filter(x -> !(x in [:seed, :auc_mse, :auc_mse_pos]), subcols)
+agcols = filter(x -> !(x in [:seed, m1, m2]), subcols)
 agdf = aggregate(subdf, agcols, [mean, std])
 
 maxrow(df, s) = df[argmax(df[!,s]),:]
 
 # now extract the results
 oc1df = filter(row -> row[:neg] == false, agdf)
-delete!(oc1df, [:auc_mse_mean, :auc_mse_std])
-rename!(oc1df, :auc_mse_pos_mean => :auc)
-rename!(oc1df, :auc_mse_pos_std => :std)
+delete!(oc1df, [m1m, m1s])
+rename!(oc1df, m2m => metric_short)
+rename!(oc1df, m2s => :std)
 
 agoc1df = vcat(map(x -> maxrow(x, :auc), groupby(oc1df, :model))...)
 delete!(agoc1df, :model_1)
@@ -58,18 +66,18 @@ agoc1df = vcat(map(i -> agoc1df[i, :], map(x -> agoc1df[!,:model] .== x, sortvec
 insertcols!(agoc1df, 2, :reg => metricvec)
 
 # finally get the output df
-oagoc1df = agoc1df[!, [:reg, :auc, :std]]
+oagoc1df = agoc1df[!, [:reg, metric_short, :std]]
 s1df = PaperUtils.df2tex(oagoc1df)
-f1 = joinpath(outpath, "oneclass_t1.tex")
+f1 = joinpath(outpath, "oneclass_"*String(m2)*".tex")
 PaperUtils.string2file(f1, s1df)
 
 # the second type of training:
 oc2df = filter(row -> row[:neg] == true, agdf)
-delete!(oc2df, [:auc_mse_pos_mean, :auc_mse_pos_std])
-rename!(oc2df, :auc_mse_mean => :auc)
-rename!(oc2df, :auc_mse_std => :std)
+delete!(oc2df, [m2m, m2s])
+rename!(oc2df, m1m => metric_short)
+rename!(oc2df, m1m => :std)
 
-agoc2df = vcat(map(x -> maxrow(x, :auc), groupby(oc2df, :model))...)
+agoc2df = vcat(map(x -> maxrow(x, metric_short), groupby(oc2df, :model))...)
 delete!(agoc2df, :model_1)
 
 sortvec = "Conv" .* ["AE", "WAAE"]
@@ -79,7 +87,7 @@ agoc2df = vcat(map(i -> agoc2df[i, :], map(x -> agoc2df[!,:model] .== x, sortvec
 insertcols!(agoc2df, 2, :reg => metricvec)
 
 # finally get the output df
-oagoc2df = agoc2df[!, [:reg, :auc, :std]]
+oagoc2df = agoc2df[!, [:reg, metric_short, :std]]
 s2df = PaperUtils.df2tex(oagoc2df)
-f2 = joinpath(outpath, "oneclass_t2.tex")
+f2 = joinpath(outpath, "oneclass_"*String(m1*".tex")
 PaperUtils.string2file(f2, s2df)
